80de23b08291a0f9bfb4bd624dffc796013fdea6
alberto.paparusso <alberto.paparusso@gmail.com>
2020-04-16 11:02:17 +0200

    added value

--- index.js
+++ index.js
@@ -7,17 +7,15 @@ class MyCounter extends HTMLElement {
         this.attachShadow({mode:"open"});
         this.render();
     }
-render () {
-    const p = document.createElement('template');
-    p.innerHTML = `
-        <div>
-            <p id="oldValue">Icrement or decrement</p>
-        </div>
-    `;
-    this.shadowRoot.appendChild(p.content.cloneNode(true));
-}
-    attributeChangedCallback(oldValue, newValue){
-        this.shadowRoot.getElementById(oldValue).innerText = newValue;
+    render () {
+        const p = document.createElement('template');
+        let value = 0;
+        p.innerHTML = `
+            <div>
+                <p id="oldValue" value=${value}>Icrement or decrement ${value}</p>
+            </div>
+        `;
+        this.shadowRoot.appendChild(p.content.cloneNode(true));
     }
 }
 customElements.define("my-counter", MyCounter);
