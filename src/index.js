02cec8bf376624543b0df6be82b442f5c6be6a27
alberto.paparusso <alberto.paparusso@gmail.com>
2020-04-22 10:35:07 +0200

    added Redux-Dev-Tools + mods with Andrea's advices

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
 import {applyMiddleware, createStore} from 'redux';
+import {createAction} from 'redux-actions';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const MODIFY_COUNTER = 'MODIFY_COUNTER';
+const modifyCounter = createAction(MODIFY_COUNTER);
 
 const paragraph = document.getElementById('value');
 
 /*creo il reducer: responsabile delle modifiche allo state, ossia risponde alle action dispatchate - RITORNA SEMPRE UN NUOVO STATE*/
-function reducers(state, action){
+function reducers(state = {counter: 0}, action){
     if(typeof state === 'undefined') {
         return 0
     }
     switch(action.type){
-        case 'INCREMENT':
-            return state = state + 1;
-        case 'DECREMENT':
-            return state = state - 1;
+        case MODIFY_COUNTER:
+            return {
+                ...state,
+                counter: state.counter + action.payload
+            };
         default:
             return state
     }
 }
 
 /*creo il redux store */
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 /*creo la funzione render per renderizzare in HTML */
 function render(){
-    paragraph.innerHTML = store.getState().toString();
-
+    paragraph.innerHTML = store.getState().counter;
 }
+
 /*renderizzo lo state quando carico la pagina*/
 render();
 
@@ -32,30 +38,21 @@ const incrButton = document.getElementById('increment');
 const decrButton = document.getElementById('decrement');
 
 incrButton.addEventListener('click', function(){
-    store.dispatch({type: 'INCREMENT'});
+    store.dispatch(modifyCounter(1));
+    console.log('State is: ' ,store.getState());
 });
 
 decrButton.addEventListener('click', function(){
-    store.dispatch({type: 'DECREMENT'});
+    store.dispatch(modifyCounter(-1));
+    console.log('State is: ' ,store.getState());
 });
 
-//faccio il subscribe per il render della value nel paragrafo a ogni aggiornamento
+//faccio il subscribe per il render della value nell'element paragraph a ogni aggiornamento
 store.subscribe(render);
 
 
 /*con il metodo getState() prendo il valore dello state*/
-console.log("STARTING STATE IS: " + store.getState());
-
-
-
-
-
-
-
-
-
-
-
+console.log("STARTING STATE IS: " + store.getState().counter);
 
 
 
@@ -91,7 +88,7 @@ const reducers = (state = 0, action) => {
 */
 
 
-/*function che dispatcha il payload 
+/*function che dispatchano il payload 
 const incrementPayload = (value) => {
     return {type: INCREMENT,
             payload: value}
@@ -101,25 +98,8 @@ const decrementPayload = (value) => {
     return {type: DECREMENT,
             payload: value}
 }
-
-
-// console.log(reducers);
-
-
-/*creo le action creator (funzioni che ritornano le azioni. Ovvero creano oggetti che rappresentano gli eventi dell'azione)
-
-  const INCRACTION = () => {
-      return {type: INCREMENT,
-              text: "You clicked the INCREMENTER!"
-             } 
-  }
-
-  const DECRACTION = () => {
-      return {type: DECREMENT,
-              text: "You clicked the DECREMENTER!"
-             }
-  }
 */
+
 
 /* invio la action allo store*/
 // console.log("dispatcho la action ",store.dispatch(INCRACTION()));
